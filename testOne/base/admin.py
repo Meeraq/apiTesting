d8b41196d267d62f7e8ed331a6b413cfab1b7f3e
Asuraking1n <nishant99tiwari@gmail.com>
2022-07-08 09:55:51 +0530

    Feat: added Batch Api and Category Api

--- testOne/base/admin.py
+++ testOne/base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Courses,Learners,Batch
+from .models import Courses,Learners,Batch,CourseCategorys
 
 admin.site.register(Courses)
 admin.site.register(Learners)
 admin.site.register(Batch)
+admin.site.register(CourseCategorys)
