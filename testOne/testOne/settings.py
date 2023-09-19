c7b6990903213a4635e008bbd6399217308590a3
pankaj-prog <wadhwanip38@gmail.com>
2023-09-19 17:30:01 +0530

    update settings - console to smtp

--- testOne/testOne/settings.py
+++ testOne/testOne/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = "testOne.urls"
 
 
 EMAIL_USE_TLS = True
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
