59633ee8d93e71e674e7b5eeea9ab8cf688c6d22
pankaj-prog <wadhwanip38@gmail.com>
2023-10-08 11:24:25 +0530

    fix:get_all_confirmed_slots, only showing unbook

--- testOne/api/views.py
+++ testOne/api/views.py
@@ -1802,8 +1802,10 @@ def get_upcoming_sessions(request):
 @api_view(["GET"])
 def get_all_confirmed_slots(request):
     today_date = datetime.now()
-    timestamp_in_milliseconds = str(int(today_date.timestamp() *1000))
-    confirmed_slots = ConfirmedSlotsbyCoach.objects.filter(start_time__gte=timestamp_in_milliseconds)
+    timestamp_in_milliseconds = str(int(today_date.timestamp() * 1000))
+    confirmed_slots = ConfirmedSlotsbyCoach.objects.filter(
+        start_time__gte=timestamp_in_milliseconds, is_confirmed=False
+    )
     coach_slots_list = []
     coach_slots_dict = {}
     for slot in confirmed_slots:
